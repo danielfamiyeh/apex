32689fcfcc1d7e90df4d4b696f78f5025df5c8af
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2021-10-12 02:48:19 +0100

    Add codes to get to leaves

--- succinct/WaveletTree.h
+++ succinct/WaveletTree.h
@@ -5,35 +5,44 @@
 #ifndef DNASSEMBLER_WAVELETTREE_H
 #define DNASSEMBLER_WAVELETTREE_H
 
+#include <deque>
+
 #include <vector>
 
+#include <unordered_map>
+
 #include "BitVector.h"
+
 #include "ISuccinctStructure.h"
+
 #include "../tree/Vertex.h"
 
-typedef Vertex<BitVector<bool>, char> WaveletNode;
+typedef Vertex < BitVector < bool > , char > WaveletNode;
 
-class WaveletTree{
+class WaveletTree {
 private:
-  Vertex<BitVector<bool>, char>* root;
-  Vertex<BitVector<bool>, char>*
+  std::unordered_map < char, std::deque< bool >> codes;
+  std::vector<WaveletNode* > leaves;
+  Vertex < BitVector < bool > , char > * root;
+  Vertex < BitVector < bool > , char > *
   partition(
-    std::string alphabet,
-    const std::string& str, bool start
-    );
+      std::string alphabet,
+      const std::string & str, bool start
+  );
+  void
+  getCodes();
 public:
   WaveletTree(
-          std::string alphabet,
-          const std::string& str
-          );
+      std::string alphabet,
+      const std::string & str
+  );
   ~WaveletTree();
-
 
   void
   preorder(
-    Vertex<BitVector<bool>, char>* v,
-    void (*fun)(Vertex<BitVector<bool>, char>*)
-    );
+      Vertex < BitVector < bool > , char > * v,
+      void( * fun)(Vertex < BitVector < bool > , char > * )
+  );
 
   void
   print();
@@ -41,9 +50,10 @@ public:
   char
   access(int i);
 
-//    int rank(T c, int i);
-//    int select(T c, int i);
-};
+  int
+  rank(char c, int i);
 
+  //    int select(T c, int i);
+};
 
 #endif //DNASSEMBLER_WAVELETTREE_H
