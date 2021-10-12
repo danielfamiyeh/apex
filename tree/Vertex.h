f5b94a46f3b4fb511ad9a6dc66a7b2e42490c222
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2021-10-13 00:12:28 +0100

    Change succinct representations to be string-based (to allow for flags)

--- tree/Vertex.h
+++ tree/Vertex.h
@@ -5,7 +5,7 @@
 #ifndef DNASSEMBLER_VERTEX_H
 #define DNASSEMBLER_VERTEX_H
 
-template <typename T, typename T2> class Vertex {
+template <typename T, typename T2, typename  T3> class Vertex {
 
 private:
   Vertex *left;
@@ -13,23 +13,23 @@ private:
   Vertex *parent;
   T *value;
   T2 *leafValue;
-  T2 *edgeValue;
+  T3 *edgeValue;
 
 public:
   explicit Vertex(T val);
   ~Vertex();
-  Vertex<T, T2> *getLeftChild();
-  Vertex<T, T2> *getRightChild();
-  Vertex<T, T2> *getParent();
+  Vertex<T, T2, T3> *getLeftChild();
+  Vertex<T, T2, T3> *getRightChild();
+  Vertex<T, T2, T3> *getParent();
   T getValue();
   T2 getLeafValue();
-  T2 getEdgeValue();
+  T3 getEdgeValue();
 
-  void setLeftChild(Vertex<T, T2> *lv);
-  void setRightChild(Vertex<T, T2> *rv);
-  void setParent(Vertex<T, T2> *pv);
+  void setLeftChild(Vertex<T, T2, T3> *lv);
+  void setRightChild(Vertex<T, T2, T3> *rv);
+  void setParent(Vertex<T, T2, T3> *pv);
   void setValue(T val);
-  void setEdgeValue(T2 edgeVal);
+  void setEdgeValue(T3 edgeVal);
   void setLeafValue(T2 val);
 };
 
