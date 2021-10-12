6961927bc6ee96e57882ef28fa8e286b5037142a
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2021-10-13 00:12:28 +0100

    Change succinct representations to be string-based (to allow for flags)

--- tree/Vertex.cpp
+++ tree/Vertex.cpp
@@ -7,77 +7,84 @@
 #include "../succinct/BitVector.h"
 
 // Constructor
-template <typename T, typename T2> Vertex<T, T2>::Vertex(T val) {
+template <typename T, typename T2, typename T3>
+Vertex<T, T2, T3>::Vertex(T val) {
   left = nullptr;
   right = nullptr;
   parent = nullptr;
   value = new T(val);
   leafValue = new T2();
-  edgeValue = new T2();
+  edgeValue = new T3();
 }
 
-template <typename T, typename T2> Vertex<T, T2>::~Vertex() {
+template <typename T, typename T2, typename T3> Vertex<T, T2, T3>::~Vertex() {
   delete (value);
   delete (edgeValue);
   delete (leafValue);
 }
 
 // Getters
-template <typename T, typename T2> T Vertex<T, T2>::getValue() {
+template <typename T, typename T2, typename T3>
+T Vertex<T, T2, T3>::getValue() {
   return *value;
 }
 
-template <typename T, typename T2> T2 Vertex<T, T2>::getEdgeValue() {
+template <typename T, typename T2, typename T3>
+T3 Vertex<T, T2, T3>::getEdgeValue() {
   return *edgeValue;
 }
 
-template <typename T, typename T2>
-Vertex<T, T2> *Vertex<T, T2>::getLeftChild() {
+template <typename T, typename T2, typename T3>
+Vertex<T, T2, T3> *Vertex<T, T2, T3>::getLeftChild() {
   return left;
 }
 
-template <typename T, typename T2>
-Vertex<T, T2> *Vertex<T, T2>::getRightChild() {
+template <typename T, typename T2, typename T3>
+Vertex<T, T2, T3> *Vertex<T, T2, T3>::getRightChild() {
   return right;
 }
 
-template <typename T, typename T2> Vertex<T, T2> *Vertex<T, T2>::getParent() {
+template <typename T, typename T2, typename T3>
+Vertex<T, T2, T3> *Vertex<T, T2, T3>::getParent() {
   return parent;
 }
 
-template <typename T, typename T2> T2 Vertex<T, T2>::getLeafValue() {
+template <typename T, typename T2, typename T3>
+T2 Vertex<T, T2, T3>::getLeafValue() {
   return *leafValue;
 }
 
 // Setters
-template <typename T, typename T2> void Vertex<T, T2>::setValue(T val) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setValue(T val) {
   *value = val;
 }
 
-template <typename T, typename T2>
-void Vertex<T, T2>::setEdgeValue(T2 edgeVal) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setEdgeValue(T3 edgeVal) {
   *edgeValue = edgeVal;
 }
 
-template <typename T, typename T2>
-void Vertex<T, T2>::setLeftChild(Vertex<T, T2> *lv) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setLeftChild(Vertex<T, T2, T3> *lv) {
   left = lv;
   lv->parent = this;
 }
 
-template <typename T, typename T2>
-void Vertex<T, T2>::setRightChild(Vertex<T, T2> *rv) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setRightChild(Vertex<T, T2, T3> *rv) {
   right = rv;
   rv->parent = this;
 }
 
-template <typename T, typename T2>
-void Vertex<T, T2>::setParent(Vertex<T, T2> *pv) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setParent(Vertex<T, T2, T3> *pv) {
   parent = pv;
 }
 
-template <typename T, typename T2> void Vertex<T, T2>::setLeafValue(T2 val) {
+template <typename T, typename T2, typename T3>
+void Vertex<T, T2, T3>::setLeafValue(T2 val) {
   *leafValue = val;
 }
 
-template class Vertex<BitVector<bool>, char>;
+template class Vertex<BitVector<bool>, std::string, bool>;
