aaaef66d80613d45921d8ca2e4b52853b8c7dd39
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2021-10-15 23:32:18 +0100

    Fix and test backward()

--- Google_tests/DeBruijnGraphTest.cpp
+++ Google_tests/DeBruijnGraphTest.cpp
@@ -10,21 +10,16 @@
 TEST (DeBruijnGraphTest, BWTMethods) {
   auto* dbg = new DeBruijnGraph(3, "../../sample-edges");
   int dest[13] {10, 4, 5, 8, 11, 8, 9, 1, 12, 1, 2, 0, 6};
-  int dest2[4] {12, 7, 10,  1};
+  int dest2[13] {11, 7, 10,  1, 1, 2, 12, 3, 3, 6, 0, 4, 8};
 
 
   for(int i=0; i<13; i++) {
     ASSERT_EQ(dbg->forward(i), dest[i]);
   }
 
-//  for(int i=0; i<4; i++) {
-//    ASSERT_EQ(dbg->backward(i), dest2[i]);
-//  }
-
-//  ASSERT_EQ(dbg->backward(1), 7);
-//  ASSERT_EQ(dbg->backward(5), 2);
-//  ASSERT_EQ(dbg->backward(4), 1);
-  ASSERT_EQ(dbg->backward(0), 11);
+  for(int i=0; i<13; i++) {
+    ASSERT_EQ(dbg->backward(i), dest2[i]);
+  }
 }
 
 TEST (DeBruijnGraphTest, GraphMethods) {
