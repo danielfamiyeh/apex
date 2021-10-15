58c09c2eb818d582c58b073c0e56d829a07c7b73
Daniel Famiyeh <58159259+danielfamiyeh@users.noreply.github.com>
2021-10-16 00:24:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "gtest/gtest.h";
+#include "Google_tests/lib/googletest/include/gtest/gtest.h";
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
